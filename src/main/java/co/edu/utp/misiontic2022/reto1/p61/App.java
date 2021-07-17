d60fb54d8d0e6f24c418af69a3efe1d12e7d9675
Carlos E Restrepo F <cerf_fx@yahoo.com>
2021-07-17 13:53:00 -0500

    commit 1 calculo intereses

--- src/main/java/co/edu/utp/misiontic2022/reto1/p61/App.java
+++ src/main/java/co/edu/utp/misiontic2022/reto1/p61/App.java
@@ -1,13 +1,13 @@
 package co.edu.utp.misiontic2022.reto1.p61;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        VentaProyecto ventaConstruccion = new VentaProyecto();
+        System.out.println(ventaConstruccion.calcularInteresSimple(12,150000,2.0));
+        System.out.println(ventaConstruccion.calcularInteresCompuesto(12,150000,2.0));
+        //System.out.println(ventaConstruccion.compararInversion(36,200000,5.0));
     }
 }
--- src/main/java/co/edu/utp/misiontic2022/reto1/p61/VentaProyecto.java
+++ src/main/java/co/edu/utp/misiontic2022/reto1/p61/VentaProyecto.java
@@ -2,30 +2,46 @@ package co.edu.utp.misiontic2022.reto1.p61;
 
 public class VentaProyecto {
 
-    double Monto;
+    public int Tiempo=0;
+    public double Monto=0;
+    public double Interes=0;
+    public double InteresSimple=0;
+    public double InteresCompuesto=0;
+    public double diferencia=0;
 
-    // -----------------------------------------------------------------
-// Atributos
-// -----------------------------------------------------------------
-// ...
-// -----------------------------------------------------------------
+
+
+
+
 // Constructores
 // -----------------------------------------------------------------
 // ...
 // -----------------------------------------------------------------
 // Métodos
 // -----------------------------------------------------------------
-public double calcularInteresSimple( ){
-    // ...
+public double calcularInteresSimple(int pTiempo, double pMonto, double pInteres){
+
+    return Math.round(InteresSimple=pMonto*(pInteres/100)*pTiempo);
+
     }
-public double calcularInteresCompuesto( ){
-    // ...
+
+public double calcularInteresCompuesto(int pTiempo, double pMonto, double pInteres ){
+
+ //   System.out.println( Math.pow(5,3));
+
+    //return Math.round(InteresCompuesto=pMonto*Math.pow((1+(pInteres/100)),pTiempo)  -1);
+
+    return Math.round (InteresCompuesto= pMonto * (Math.pow (  (1+(pInteres/100)  ) , pTiempo  )  -1));
+
     }
-public String compararInversion (int pTiempo, double pMonto, double pInteres){
+
+/*public String compararInversion (int pTiempo, double pMonto, double pInteres){
     // ...
     }
 public String compararInversion ( ){
     // ...
     }
+
+    */
     
 }
