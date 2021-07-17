7cfc5dfc442ef24209ba1397ae098110d2ffd5c8
Carlos E Restrepo F <cerf_fx@yahoo.com>
2021-07-17 12:06:07 -0500

    commit 1 Inicial

---
+++ src/main/java/co/edu/utp/misiontic2022/reto1/p61/App.java
@@ -0,0 +1,13 @@
+package co.edu.utp.misiontic2022.reto1.p61;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/co/edu/utp/misiontic2022/reto1/p61/VentaProyecto.java
@@ -0,0 +1,29 @@
+package co.edu.utp.misiontic2022.reto1.p61;
+
+public class VentaProyecto {
+
+    // -----------------------------------------------------------------
+// Atributos
+// -----------------------------------------------------------------
+// ...
+// -----------------------------------------------------------------
+// Constructores
+// -----------------------------------------------------------------
+// ...
+// -----------------------------------------------------------------
+// Métodos
+// -----------------------------------------------------------------
+public double calcularInteresSimple( ){
+    // ...
+    }
+public double calcularInteresCompuesto( ){
+    // ...
+    }
+public String compararInversion (int pTiempo, double pMonto, double pInteres){
+    // ...
+    }
+public String compararInversion ( ){
+    // ...
+    }
+    
+}
