71bc55d190c33b0dfa9c29655097fd9ef7386a38
Carlos e Restrepo F <cerf_fx@yahoo.com>
2021-07-24 14:20:30 -0500

    commit proceso clase reto 1

--- src/main/java/co/edu/utp/misiontic2022/reto1/p61/VentaProyecto.java
+++ src/main/java/co/edu/utp/misiontic2022/reto1/p61/VentaProyecto.java
@@ -5,79 +5,59 @@ public class VentaProyecto {
     public int Tiempo;
     public double Monto;
     public double Interes;
-    public double InteresSimple;
+
     public double InteresCompuesto;
     public double diferencia;
     
 
 
 public VentaProyecto() {
-        Tiempo = 0;
-        Monto = 0;
-        Interes = 0;
+        this.Tiempo = 0;
+        this.Monto = 0;
+        this.Interes = 0;
 
     }
 
+public VentaProyecto(int tiempo, double monto, double interes) {
+        this.Tiempo = tiempo;
+        this.Monto = monto;
+        this.Interes = interes;
 
-
-
-
-public VentaProyecto(int tiempo, double monto, double interes, double interesSimple, double interesCompuesto,
-            double diferencia) {
-        Tiempo = tiempo;
-        Monto = monto;
-        Interes = interes;
-        InteresSimple = interesSimple;
-        InteresCompuesto = interesCompuesto;
-        this.diferencia = diferencia;
     }
-
-
-
-public double calcularInteresSimple(int pTiempo, double pMonto, double pInteres){
-
-    return Math.round(InteresSimple=pMonto*(pInteres/100)*pTiempo);
-    }
-
 
 
 public double calcularInteresSimple(){
-
-    return InteresSimple=0;
+    double InteresSimple = Math.round(InteresSimple=this.Monto*(this.Interes/100)*this.Tiempo);
+    return InteresSimple;
     }
-
-
-
-
-public double calcularInteresCompuesto(int pTiempo, double pMonto, double pInteres ){
-
-    return InteresCompuesto= pMonto * (Math.pow (  (1+(pInteres/100)  ) , pTiempo  )  -1);
-    }
-
-
 
 public double calcularInteresCompuesto(){
+        double InteresCompuesto = Math.round(InteresCompuesto= this.Monto * (Math.pow (  (1+(this.Interes/100)  ) , this.Tiempo  )  -1));
+        return InteresCompuesto;
+        }
 
-    return InteresCompuesto=0;
-    }
 
-
+public String compararInversion ( ){
+            return "No se obtuvo diferencia entre las proyecciones, revisar los parámetros de entrada.";
+          }
 
 
 public String compararInversion (int pTiempo, double pMonto, double pInteres){
-    
+ 
+    diferencia = this.calcularInteresCompuesto() - this.calcularInteresSimple() ;
+    return  "La diferencia entre la proyección de interés compuesto e interés simple es: $" + diferencia ;
+}
+
+    /*
     diferencia= (Math.round (InteresCompuesto= pMonto * (Math.pow (  (1+(pInteres/100)  ) , pTiempo  )  -1)) - 
-    (Math.round(InteresSimple=pMonto*(pInteres/100)*pTiempo)));
+    (Math.round( InteresSimple = pMonto*(pInteres/100)*pTiempo)));
 
     return  "La diferencia entre la proyección de interés compuesto e interés simple es: $" + diferencia ;
 
     }
 
+*/
 
-public String compararInversion ( ){
-
-    return "No se obtuvo diferencia entre las proyecciones, revisar los parámetros de entrada.";
-  }
 
     
     
